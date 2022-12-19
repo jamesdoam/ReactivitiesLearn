631044e86fa0270a3f4aa6092b40ac541676a876
tnguyen2620 <jamesdoam@gmail.com>
2022-12-19 23:20:39 +1100

    S2 - End - Create a basic Activities Controller

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,7 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
@@ -22,14 +22,15 @@ namespace API
             try 
             {
                 var context = services.GetRequiredService<DataContext>();
-                context.Database.Migrate();                
+                await context.Database.MigrateAsync();   
+                await Seed.SeedData(context);             
             }
             catch (Exception ex)
             {
                 var logger = services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex, "An error occured during migraiton");
             }            
-            host.Run();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
