820c766cba89e4d68f15d9b956d665df3f2e7ad2
tnguyen2620 <jamesdoam@gmail.com>
2022-12-23 08:40:46 +1100

    S4 - 35 Create first query handler + thin control

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,27 +9,23 @@ using Microsoft.Extensions.Logging;
 using Persistence;
 using Domain;
 using Activity = Domain.Activity;
+using MediatR;
+using Application.Activities;
 
 namespace API.Controllers
 {
      public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id) 
         {
-            return await _context.Activities.FindAsync(id);
+            return Ok();
         }
     }
 }
