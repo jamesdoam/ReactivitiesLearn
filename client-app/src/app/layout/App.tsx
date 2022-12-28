53c22dbba332e0a3cd542f60b2d847bb2a3e38a5
tnguyen2620 <jamesdoam@gmail.com>
2022-12-28 15:43:06 +1100

    S5 - 49 Styling the NavBar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 
@@ -15,13 +15,15 @@ function App(): JSX.Element {
   return (
     <div>
       <NavBar/>
+      <Container style={{marginTop: '7em'}}>
         <List>
-          {activities.map((activity) => (
-              <List.Item key = {activity.id}>
-                  {activity.title}
-              </List.Item>
-            ))}
-        </List>
+            {activities.map((activity) => (
+                <List.Item key = {activity.id}>
+                    {activity.title}
+                </List.Item>
+              ))}
+          </List>
+      </Container>        
     </div>
   );
 }
