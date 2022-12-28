406d47d72121dd263fff28cdc3ee268b43c57c16
tnguyen2620 <jamesdoam@gmail.com>
2022-12-28 22:20:02 +1100

    S5 - 54 Select and Unselect an activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,17 +7,32 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 
 function App(): JSX.Element {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity|undefined>(undefined);
+
   useEffect(()=>{
     axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
       console.log(response);
       setActivities(response.data);
     })
   },[]);
+
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectedActivity(){
+    setSelectedActivity(undefined);
+  }
   return (
     <Fragment>
       <NavBar/>
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard activities = {activities}/>
+        <ActivityDashboard 
+          activities = {activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectedActivity}
+        />
       </Container>        
     </Fragment>
   );
