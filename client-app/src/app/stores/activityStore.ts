343fce6f2ca7464be027a681efe7cb331f2395fb
tnguyen2620 <jamesdoam@gmail.com>
2022-12-31 08:01:43 +1100

    S7 - 73 Mobx strict mode

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable} from "mobx";
+import {makeAutoObservable} from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
 
@@ -14,18 +14,23 @@ export default class activityStore {
     }
 
     loadActivities = async () => {
-        this.loadingInitial = true; 
+        this.setLoadingInitial(true);
         try {
             const activities = await agent.Activities.list();
+            
             activities.forEach(activity => {
                 activity.date = activity.date.split('T')[0];
                 this.activities.push(activity);
             })
-            this.loadingInitial = false;
+            this.setLoadingInitial(false);
         } catch (error){
             console.log(error);
-            this.loadingInitial=false;
+            this.setLoadingInitial(false);            
         }
+    }
+
+    setLoadingInitial = (state:boolean)=> {
+        this.loadingInitial =state;
     }
 
     /* setTitle = () => {
