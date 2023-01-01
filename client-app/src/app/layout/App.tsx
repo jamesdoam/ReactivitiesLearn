85e95ecdc6417fa52758d948d3b4bbf3163e409f
tnguyen2620 <jamesdoam@gmail.com>
2023-01-01 23:09:13 +1100

    S7 - 76 Delete an activity using Mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,7 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import agent from '../api/agent';
 import LoandingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
@@ -11,23 +9,10 @@ import { observer } from 'mobx-react-lite';
 function App(): JSX.Element {
 
   const {activityStore} = useStore();
-  
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [submitting, setSubmitting ] = useState(false);
 
   useEffect(()=>{
     activityStore.loadActivities();
   },[activityStore]);
-
-  function handleDeleteActivity(id: string){
-    setSubmitting(true);
-    agent.Activities.delete(id).then(()=>{
-      setActivities([...activities.filter(x => x.id !== id)]);
-      setSubmitting(false);
-    })    
-  }
-
-
 
   if (activityStore.loadingInitial) return <LoandingComponent content='Loading app'/>
 
@@ -35,11 +20,7 @@ function App(): JSX.Element {
     <Fragment>
       <NavBar/>
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard 
-          activities = {activityStore.activities}
-          deleteActivity = {handleDeleteActivity}
-          submitting = {submitting}
-        />
+        <ActivityDashboard/>
       </Container>        
     </Fragment>
   );
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,22 +1,19 @@
+import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent, useState } from 'react'
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity'
 import { useStore } from '../../../app/stores/store';
 
-interface Props {
-    activities: Activity[];
-    deleteActivity: (id:string) => void;
-    submitting:boolean;
-}
+export default observer(function ActivityList() {
+    const {activityStore} = useStore();
+    const {deleteActivity, activities, loading} = activityStore;
 
-export default function ActivityList({activities, deleteActivity, submitting}:Props) {
     const [target,setTarget] = useState(''); /*this is to solve the issue of all Delete buttons loading*/
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>,id:string){
         setTarget(e.currentTarget.name);
         deleteActivity(id);
     }
 
-    const {activityStore} = useStore();
+    
     return (
         <Segment>
             <Item.Group divided>
@@ -35,7 +32,7 @@ export default function ActivityList({activities, deleteActivity, submitting}:Pr
                                 <Button onClick={() => activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue'/>
                                 <Button 
                                     name={activity.id}
-                                    loading={submitting && target === activity.id} 
+                                    loading={loading && target === activity.id} 
                                     onClick={(e) => handleActivityDelete(e, activity.id)} 
                                     floated='right' 
                                     content='Delete' 
@@ -49,4 +46,4 @@ export default function ActivityList({activities, deleteActivity, submitting}:Pr
             </Item.Group>
         </Segment>
     )
-}
+})
